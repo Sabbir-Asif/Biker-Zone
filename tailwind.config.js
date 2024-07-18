79e3c471b222563e074ca076d15d334e5ef294d0
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2024-07-18 16:40:58 +0600

    added nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    //...
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
     plugins: [
-      require('daisyui'),
-    ],
+        require('daisyui'),
+      ],
   }
